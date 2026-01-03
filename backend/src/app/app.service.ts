69787cc471a22e4dd84bd411221c984e2797f8e4
w1qdev <daniilzemla@gmail.com>
2026-01-03 19:30:14 +0500

    fix: refactor rate limiting implementation and improve error handling in app service

--- backend/src/app/app.service.ts
+++ backend/src/app/app.service.ts
@@ -1,16 +1,15 @@
 import cors from 'cors'
-import express, { type Express } from 'express'
+import express, { type Express, type Request, type Response } from 'express'
 import helmet from 'helmet'
 import authModule from '../auth/auth.module.ts'
 import UserModule from '../users/users.module.ts'
 import { LoggerService } from '../utils/logger.service.ts'
-import { RateLimitRequestHandler, rateLimit } from 'express-rate-limit'
+import { rateLimit } from 'express-rate-limit'
 
 export class AppService {
     private app: Express | null
     private PORT: number | string
     private logger: LoggerService
-    private rateLimit: RateLimitRequestHandler
 
     constructor(port: number = 3000) {
         this.app = express()
@@ -19,28 +18,32 @@ export class AppService {
     }
 
     run(callbackFn?: () => void) {
-        this.app?.use(express.json())
-        this.app?.use(helmet())
-        this.app?.use(
+        if (!this.app) return
+
+        this.app.use(express.json())
+        this.app.use(helmet())
+        this.app.use(
             cors({
-                origin: 'http://localhost:5173',
+                origin: process.env.CLIENT_URL || 'http://localhost:5173',
                 credentials: true,
             })
         )
-        this.app?.use(this.rateLimit{
-            windowMs: 15 * 60 * 1000,
-            max: 100,
-            message: 'Слишком много запросов, попробуйте позже.',
-            headers: true,
-            standardHeaders: true,
-            legacyHeaders: false,
-            skip: (req: Request, res: Response) => {
-                return req.method === 'OPTIONS'
-            },
-        })
 
-        this.app?.use('/api/auth', authModule())
-        this.app?.use('/api/users', UserModule())
+        this.app.use(
+            rateLimit({
+                windowMs: 15 * 60 * 1000,
+                max: 100,
+                message: 'Слишком много запросов, попробуйте позже.',
+                standardHeaders: true,
+                legacyHeaders: false,
+                skip: (req: Request, res: Response) => {
+                    return req.method === 'OPTIONS'
+                },
+            })
+        )
+
+        this.app.use('/api/auth', authModule())
+        this.app.use('/api/users', UserModule())
 
         if (this.app) {
             this.app.listen(this.PORT, callbackFn)
